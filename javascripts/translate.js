86dfd112d3f54dc34d489c502a5081e943f14221
Kevin Bian <kaiwenbian107@gmail.com>
2024-06-09 01:58:17 -0700

    add more languages and adjust tool bar

--- javascripts/translate.js
+++ javascripts/translate.js
@@ -1,8 +1,21 @@
+// function showTranslateOptions() {
+//     var translateElement = document.getElementById('google_translate_element');
+//     if (translateElement.style.display === 'block') {
+//         translateElement.style.display = 'none';
+//     } else {
+//         translateElement.style.display = 'block';
+//     }
+// }
+
 function showTranslateOptions() {
     var translateElement = document.getElementById('google_translate_element');
+    var navList = document.querySelector('.nav-list');
+
     if (translateElement.style.display === 'block') {
         translateElement.style.display = 'none';
+        navList.style.top = '0'; // Resets to original top position when toolbar is hidden
     } else {
         translateElement.style.display = 'block';
+        navList.style.top = '30px'; // Moves down to make space for the translate bar, adjust as needed
     }
 }
