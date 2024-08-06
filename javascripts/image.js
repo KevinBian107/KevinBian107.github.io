633a5b22d49883eea326e1142bea23cd856fec89
Kevin Bian <kaiwenbian107@gmail.com>
2024-08-05 17:14:46 -0700

    added more profile picture

---
+++ javascripts/image.js
@@ -0,0 +1,25 @@
+// Array of image sources
+const images = [
+    'assets/profile_kaiwen_picture_3.jpg',
+    'assets/profile_kaiwen_picture_2.jpg'
+    // Add more image sources here
+];
+
+function getRandomImage() {
+    const index = Math.floor(Math.random() * images.length);
+    return images[index];
+}
+
+function setRandomImages() {
+    let img1, img2;
+    do {
+        img1 = getRandomImage();
+        img2 = getRandomImage();
+    } while (img1 === img2); // Ensure the two images are different
+
+    document.getElementById('image1').src = img1;
+    document.getElementById('image2').src = img2;
+}
+
+// Set random images on page load
+window.onload = setRandomImages;
